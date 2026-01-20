6eb2a9803be07823a303c94a94ee0d0f4bcbaf3d
giorgos-grekis <georgrekis.github@gmail.com>
2026-01-20 22:19:56 +0200

    add state to coin detail and coins view model

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,49 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_list
+
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.domain.use_case.get_coins.GetCoinsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onEmpty
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel @Inject constructor(
+    private val getCoinsUseCase: GetCoinsUseCase
+): ViewModel() {
+
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        // we can call the like a function
+        getCoinsUseCase().onEach { result ->
+            when(result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(coins = result.data ?: emptyList())
+                }
+
+                is Resource.Error -> {
+                    _state.value = CoinListState(
+                        error = result.message ?: "An unexpected error occurred"
+                    )
+                }
+
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
