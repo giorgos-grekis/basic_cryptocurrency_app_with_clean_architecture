f9beb181fb5f3754283c153e1ff2db970310462a
giorgos-grekis <georgrekis.github@gmail.com>
2026-01-20 16:57:53 +0200

    create getCoinUseCase

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,31 @@
+package com.plcoding.cryptocurrencyappyt.domain.use_case.get_coins
+
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoin
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+
+    // We overwrite the invoke function
+    // so we can call the GetCoinsUseCase as if it was a function
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+
+}
