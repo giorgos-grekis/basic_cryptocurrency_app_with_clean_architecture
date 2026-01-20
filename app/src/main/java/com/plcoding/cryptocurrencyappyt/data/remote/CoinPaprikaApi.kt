3b11c937bec3b19da4803972472ac92913f9bd0a
giorgos-grekis <georgrekis.github@gmail.com>
2026-01-20 16:06:19 +0200

    add the repositories

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaApi.kt
@@ -1,24 +1,15 @@
 package com.plcoding.cryptocurrencyappyt.data.remote
 
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDetailDto
 import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-class CoinPaprikaApi {
+abstract class CoinPaprikaApi {
 
-//    https://api.coinpaprika.com/v1/coins
-
-//    {
-    //    "id": "btc-bitcoin",
-    //    "name": "Bitcoin",
-    //    "symbol": "BTC",
-    //    "rank": 1,
-    //    "is_new": false,
-    //    "is_active": true,
-    //    "type": "coin"
-//     },
     @GET("/v1/coins")
     suspend fun getCoins(): List<CoinDto>
 
     @GET("/v1/coins/{coinId}")
-    suspend fun getCoinById(@Path("coinId") coindId: String):
+    suspend fun getCoinById(@Path("coinId") coindId: String): CoinDetailDto
 }
